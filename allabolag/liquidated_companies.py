6c0f04bb66b2982ccc52553f57b77d30a0276f72
jensfinnas <jens.finnas@gmail.com>
2019-12-03 09:36:33 +0100

    allabolag changed site structure

--- allabolag/liquidated_companies.py
+++ allabolag/liquidated_companies.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-from allabolag.utils import _dl_to_dict
+from copy import deepcopy
 import re
-
+import json
+from allabolag.utils import _dl_to_dict
 
 
 def iter_liquidated_companies(until):
@@ -17,15 +18,21 @@ def iter_liquidated_companies(until):
         until = datetime.strptime(until, "%Y-%m-%d")
     page = 1
     has_more_results = True
+
     while has_more_results:
         url = "https://www.allabolag.se/lista/konkurs-inledd/6/?page={}".format(page)
         print("/GET {}".format(url))
         r = requests.get(url)
         r.raise_for_status()
         soup = BeautifulSoup(r.content, "html.parser")
-        for item in soup.select(".search-results__item"):
-            item_data = _parse_liquidated_company_item(item)
+        data = json.loads(soup.select_one(".page.search-results search")\
+                              .attrs[":search-result-default"])
 
+        if len(data) == 0:
+            raise Exception(f"No results on {url}")
+
+        for item in data:
+            item_data = _parse_liquidated_company_item(item)
             if item_data["Konkurs inledd"] < until:
                 has_more_results = False
             else:
@@ -33,18 +40,15 @@ def iter_liquidated_companies(until):
 
         page += 1
 
-def _parse_liquidated_company_item(item_soup):
-    item = {}
+def _parse_liquidated_company_item(item_dict):
+    item = deepcopy(item_dict)
 
-    a_tag = item_soup.select_one("a")
-    item["link"] = a_tag["href"]
-    item["Namn"] = a_tag.text.strip()
+    # store for backward compability
+    item["link"] = item_dict["linkTo"]
+    item["Org.nummer"] = item_dict["orgnr"]
 
-    date_str = re.search(r"Konkurs inledd (\d{4}-\d{2}-\d{2})", item_soup.text).group(1)
-    item["Konkurs inledd"] = datetime.strptime(date_str, "%Y-%m-%d")
-
-    # org nr & branch
-    details = _dl_to_dict(item_soup.select_one(".search-results__item__details"))
-    item.update(details)
+    for remark in item_dict["remarks"]:
+        key = remark["remarkDescription"] # ie. Konkurs inledd
+        item[key] = datetime.strptime(remark["remarkDate"], "%Y-%m-%d")
 
     return item
