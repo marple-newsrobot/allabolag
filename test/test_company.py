d15f5008c6439c94e6b4c4ab90611cbfe405da89
rotsee <mejl@leowallentin.se>
2022-02-17 08:52:14 +0100

    test that we have some content

--- test/test_company.py
+++ test/test_company.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 from allabolag import Company
 
+
 def test_company():
     c = Company("559071-2807")
     data = c.data
     assert(isinstance(data, dict))
+    assert data["Översikt - Namn"] == "Journalism Robotics Stockholm AB"
