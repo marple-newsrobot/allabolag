06febe3dd29e37ccb3d96140dd23afcea36add24
rotsee <mejl@leowallentin.se>
2022-03-09 12:58:11 +0100

    test company properties

--- test/test_company.py
+++ test/test_company.py
@@ -8,8 +8,18 @@ def test_company():
     assert(isinstance(data, dict))
     assert data["Översikt - Namn"] == "Journalism Robotics Stockholm AB"
 
+    c = Company("5590712807")
+    data = c.data
+    assert(isinstance(data, dict))
+    assert data["Översikt - Namn"] == "Journalism Robotics Stockholm AB"
+
 
 def test_invalid_company():
     with pytest.raises(NoSuchCompany):
         c = Company("559071-xxxx")
         c.data
+
+
+def test_liquidated():
+    c = Company("5569867632")
+    assert c.liquidated
