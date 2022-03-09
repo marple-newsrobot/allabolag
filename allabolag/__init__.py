2cb34a0b6b9611719b56668abb6a2a6991b2bd0e
rotsee <mejl@leowallentin.se>
2022-03-09 12:20:25 +0100

    declare __all__, as per PEP8

--- allabolag/__init__.py
+++ allabolag/__init__.py
@@ -1,3 +1,4 @@
 from allabolag.company import Company, NoSuchCompany
 from allabolag.liquidated_companies import iter_liquidated_companies
 from allabolag.list import iter_list
+__all__ = [Company, NoSuchCompany, iter_liquidated_companies, iter_list]
