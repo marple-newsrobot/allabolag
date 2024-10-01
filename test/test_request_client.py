52a6cf5ad6ca933cec684e8b82dbdeb3fd437358
jensfinnas <jens.finnas@gmail.com>
2024-10-01 11:53:54 +0200

    move to avoid delete

--- test/test_request_client.py
+++ test/test_request_client.py
@@ -3,6 +3,8 @@ from allabolag.request_client import RequestsRequestClient, AWSGatewayRequestCli
 from allabolag.company import Company
 from allabolag.liquidated_companies import iter_liquidated_companies
 
+ip_rot_request_client = AWSGatewayRequestClient()
+
 def test_requests_client_with_real_company():
     client = RequestsRequestClient()
     url = f"https://www.allabolag.se/5590712807"
@@ -12,22 +14,19 @@ def test_requests_client_with_real_company():
     assert response.status_code == 200
 
 def test_rotating_ip_client_with_real_company():
-    client = AWSGatewayRequestClient()
     url = f"https://www.allabolag.se/5590712807"
     
-    response = client.get(url)
+    response = ip_rot_request_client.get(url)
     
     assert response.status_code == 200
 # ... existing code ...
 
 def test_company_with_rotating_ip_client():
-    request_client = AWSGatewayRequestClient()
-    company = Company("559071-2807", request_client=request_client)
+    company = Company("559071-2807", request_client=ip_rot_request_client)
     assert company.data is not None
 
 def test_iter_liquidated_companies_with_rotating_ip_client():
-    request_client = AWSGatewayRequestClient()
-    for c in iter_liquidated_companies("1900-01-01", request_client=request_client):
+    for c in iter_liquidated_companies("1900-01-01", request_client=ip_rot_request_client):
         assert isinstance(c, dict)
         assert "orgnr" in c
         break
